0a1faa10a76a901cbb5fe470787d99b063d7d9aa
Marco De Felice <marco.defelice@qwentes.it>
2023-09-25 09:41:20 +0200

    feat: fix on verify button

--- src/components/Auth/VerifyEmailButton.vue
+++ src/components/Auth/VerifyEmailButton.vue
@@ -1,12 +1,20 @@
 <!-- LogoutButton.vue -->
 <template>
-    <button @click="verify">Send me verify link</button>
+    <div>
+        <span v-if="message">We have just sended an email</span>
+        <button @click="verify">Send me verify link</button>
+    </div>
 </template>
   
 <script>
 import AuthServiceVue from '../../services/AuthService.vue';
 
 export default {
+    data() {
+        return {
+            message: false
+        }
+    },
     props: {
         email: {
             type: String
@@ -15,6 +23,7 @@ export default {
     methods: {
         async verify() {
             const email = this.email
+            this.message = true
             AuthServiceVue.verify(email) // Adjust the URL as needed
                 .catch(error => {
                     console.error(error);
