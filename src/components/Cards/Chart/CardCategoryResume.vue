8cf6a50af1f175e2700f1372c6ac5ea425a01393
Marco De Felice <marco.defelice@qwentes.it>
2023-10-05 19:09:38 +0200

    added new label

--- src/components/Cards/Chart/CardCategoryResume.vue
+++ src/components/Cards/Chart/CardCategoryResume.vue
@@ -7,13 +7,13 @@
             {{ title }}
           </h3>
         </div>
-        <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+        <!-- <div class="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
           <a :href="'/see_all/' + path"
             class="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
             type="button">
             See all
           </a>
-        </div>
+        </div> -->
       </div>
     </div>
     <div class="block w-full overflow-x-auto">
