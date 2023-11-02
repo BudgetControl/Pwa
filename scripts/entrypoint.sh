04870e7526cfad94d6cbff9a26dde048efc0be63
Marco De Felice <marco.defelice@qwentes.it>
2023-11-02 10:00:58 +0100

    configuration

--- scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-service apache2 restart
-echo "[INFO] install Isaia application"
-cd /var/www/html && composer install -v
-echo "[INFO]  install Isaia library"
-cd /var/www/html/library && composer install -v
-echo "[INFO] log"
-tail -f -n 100 /var/www/logs/error.log
