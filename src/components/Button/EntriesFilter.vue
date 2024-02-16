46fa9b04ee1514ce29da91e52f11bdbdfaad0eec
Marco De Felice <marco.defelice@mlabfactory.it>
2024-02-16 12:35:34 +0100

    fix css

--- src/components/Button/EntriesFilter.vue
+++ src/components/Button/EntriesFilter.vue
@@ -3,21 +3,21 @@
     <ul class="relative flex px-5 py-5 navbar-expand-lg text-center">
     <li class="flex-l w-full m-1">
         <input @click="showPlanned()" type="checkbox" id="show-planned" value="" class="hidden peer" required="" v-model="isCheckedPlanned">
-        <label for="show-planned" class="w-full  uppercase text-xs  p-2 bg-gray-100 border-2 cursor-pointer"
+        <label for="show-planned" class="w-full  uppercase text-xs inline-flex p-2 bg-gray-100 border-2 cursor-pointer"
         :class="{ 'border-emerald-600': isCheckedPlanned === true }">                           
         planned
         </label>
     </li>
     <li class="flex-l w-full m-1">
         <input @click="showTransfer()" type="checkbox" id="show-transfer" value="" class="hidden peer"  v-model="isCheckedTransfer">
-        <label for="show-transfer" class="w-full  uppercase text-xs  p-2 bg-gray-100 border-2 cursor-pointer"
+        <label for="show-transfer" class="w-full  uppercase text-xs inline-flex p-2 bg-gray-100 border-2 cursor-pointer"
         :class="{ 'border-emerald-600': isCheckedTransfer === true }">  
             transfer
         </label>
     </li>
     <li class="flex-l w-full m-1">
         <input @click="showDebit()" type="checkbox" id="show debit" value="" class="hidden peer"  v-model="isCheckedDebit">  
-        <label for="show debit" class="w-full uppercase text-xs  p-2 bg-gray-100 border-2 cursor-pointer"
+        <label for="show debit" class="w-full uppercase text-xs inline-flex p-2 bg-gray-100 border-2 cursor-pointer"
         :class="{ 'border-emerald-600': isCheckedDebit === true }" >
             debit
         </label>
