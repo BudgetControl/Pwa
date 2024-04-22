eae0fe10b716da8bd7b6259630e477a940d69e04
Marco De Felice <marco.defelice@qwentes.it>
2024-04-22 18:21:44 +0200

    fix

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -135,7 +135,7 @@ export default {
 
       AuthService.login(email, password).then((response) => {
         //save token in local storage
-        LocalStorageService.setToken(response.access_token);
+        LocalStorageService.setToken(response.token);
         //redirecto to dashboard
         this.$router.push({ path: '/app/dashboard' })
       }).catch((err) => {
