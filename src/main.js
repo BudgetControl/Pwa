edcc0d8bebedc6d0699338b34af4d3ba11a58fef
Marco De Felice <marco.defelice@qwentes.it>
2024-05-24 21:15:27 +0200

    try pwa

--- src/main.js
+++ src/main.js
@@ -62,6 +62,19 @@ import AuthToken from "@/views/auth/Token.vue";
 import Landing from "@/views/Landing.vue";
 // import Index from "@/views/Index.vue";
 
+// 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
+
+
+
 // routes
 
 const routes = [
