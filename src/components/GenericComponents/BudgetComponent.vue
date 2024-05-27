cec808e12a7e9048ac0d7ecc1bc4e7752fadf565
Marco De Felice <marco.defelice@qwentes.it>
2024-05-27 10:19:29 +0200

    hotfix- budget component

--- src/components/GenericComponents/BudgetComponent.vue
+++ src/components/GenericComponents/BudgetComponent.vue
@@ -1,34 +1,34 @@
 <template>
     <div class="relative pt-1">
-        <router-link :to="'/app/budgets/edit/' + budget.budget.uuid" class="relative pt-1">
+        <router-link :to="'/app/budgets/edit/' + localBudget.localBudget.uuid" class="relative pt-1">
             <div class="flex mb-2 items-center justify-between">
                 <div>
                     <span
                         class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-emerald-600 bg-emerald-200">
-                        {{ budget.budget.name }} {{ budget.budget.amount }} {{ currency }}
+                        {{ localBudget.localBudget.name }} {{ localBudget.localBudget.amount }} {{ currency }}
                     </span>
                 </div>
                 <div class="text-right">
                     <span class="text-xs font-semibold inline-block text-blueGray-400 ml-10">
-                        {{ budget.totalRemaining }}{{ currency }}
+                        {{ localBudget.totalRemaining }}{{ currency }}
                     </span>
 
                     <span class="text-xs font-semibold inline-block text-emerald-600">
-                         {{ budget.totalRemainingPercentage }}
+                         {{ localBudget.totalRemainingPercentage }}
                     </span>
                 </div>
             </div>
             <div class="overflow-hidden h-2 mb-4 text-xs flex rounded"
-                v-bind:class="{ 'bg-emerald-200 ': budget.percentage <= 80, 'bg-red-200': budget.percentage >= 80 }">
-                <div :style="'width:' + budget.totalSpentPercentage"
+                v-bind:class="{ 'bg-emerald-200 ': localBudget.percentage <= 80, 'bg-red-200': localBudget.percentage >= 80 }">
+                <div :style="'width:' + localBudget.totalSpentPercentage"
                     class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center"
-                    v-bind:class="{ 'bg-emerald-600 ': budget.percentage <= 80, 'bg-red-600': budget.percentage >= 80 }">
+                    v-bind:class="{ 'bg-emerald-600 ': localBudget.percentage <= 80, 'bg-red-600': localBudget.percentage >= 80 }">
                 </div>
             </div>
 
             <div class="mb-4">
                 <span class="text-xs font-semibold inline-block text-blueGray-400">
-                    {{ budget.description }}
+                    {{ localBudget.description }}
                 </span>
             </div>
         </router-link>
@@ -47,9 +47,14 @@ export default {
             required: true
         }
     },
+    data() {
+    return {
+      localBudget: { ...this.budget }
+    };
+  },
     mounted() {
-        const data = this.budget
-        this.budget.totalRemaining = data.totalRemaining < 0 ? 0 : data.totalRemaining
+        const data = this.localBudget;
+        this.localBudget.totalRemaining = data.totalRemaining < 0 ? 0 : data.totalRemaining;
     }
 }
 </script>
