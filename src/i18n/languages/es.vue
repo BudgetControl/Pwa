67065a62c3e37eeb10c360c6ae996d7f6f5f18ae
Marco De Felice <marco.defelice@qwentes.it>
2024-05-31 15:49:13 +0200

    new translations

--- src/i18n/languages/es.vue
+++ src/i18n/languages/es.vue
@@ -2,12 +2,13 @@
 import messages from '../es/messages.vue';
 import labels from '../es/labels.vue';
 import text from '../es/text.vue';
-
+import app from '../es/app.vue';
 
 export default 
   {
     messages: messages,
     labels: labels,
-    text: text
+    text: text,
+    app: app
   }
 </script>
--- src/i18n/languages/it.vue
+++ src/i18n/languages/it.vue
@@ -1,12 +1,14 @@
 <script>
 import messages from '../it/messages.vue';
 import labels from '../it/labels.vue';
-import text from '../es/text.vue';
+import text from '../it/text.vue';
+import app from '../it/app.vue';
 
 export default 
   {
     messages: messages,
     labels: labels,
-    text: text
+    text: text,
+    app: app
   }
 </script>
