7ac80d3de427258204d243ca7f14aa8b2e84cd8d
Marco De Felice <marco.defelice@qwentes.it>
2024-06-11 16:28:57 +0200

    hotfix on categories translations

--- src/components/Cards/CardSearchEntries.vue
+++ src/components/Cards/CardSearchEntries.vue
@@ -91,9 +91,8 @@
                             <select multiple
                                 class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                                 v-model="action.category">
-                                <option v-for="category in input.category" :key="category.id" :value="category.id">{{
-                                    category.name
-                                    }}
+                                <option v-for="category in input.category" :key="category.id" :value="category.id">
+                                {{ $t('app.' + category.slug)}}
                                 </option>
                             </select>
                         </div>
