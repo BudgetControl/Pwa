1bb88c25efda7586d524f64195aa839e23cbc9df
Marco De Felice <marco.defelice@qwentes.it>
2024-06-11 16:47:54 +0200

    removed filter bar on home page

--- src/views/application/Dashboard.vue
+++ src/views/application/Dashboard.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="mt-10">
-    <div class="py-5">
-      <FilterBar />
-    </div>
     <div class="flex flex-wrap">
       <div class="w-full">
         <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded">
@@ -29,7 +26,6 @@
   </div>
 </template>
 <script>
-import FilterBar from "@/components/Navbars/FilterBar.vue";
 import CardBarChart from "@/components/Cards/Chart/WidgetBarChart.vue";
 import CardLine_IncomingExpensesChart from "@/components/Cards/Chart/WidgetLine_IncomingExpensesChart.vue";
 import CardPieLabelChart from "../../components/Cards/Chart/WidgetPieLabelChart.vue";
@@ -43,7 +39,6 @@ import AuthService from "../../services/AuthService.vue";
 export default {
   name: "dashboard-page",
   components: {
-    FilterBar,
     CardBarChart,
     CardLine_IncomingExpensesChart,
     CardPieLabelChart,
