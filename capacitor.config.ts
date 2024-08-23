cb86e25101e63924e16ad099a5e1de42daaf8fc9
Marco De Felice <marco.defelice@qwentes.it>
2024-08-23 17:20:29 +0200

    first config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.budgetcontrol.cloud',
+  appName: 'BudgetControl',
+  webDir: 'dist'
+};
+
+export default config;
