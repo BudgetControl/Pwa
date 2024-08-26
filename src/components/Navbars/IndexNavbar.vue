0238d7a54b767c03bccca82a7076c5e369bc2660
Marco De Felice <marco.defelice@qwentes.it>
2024-08-26 16:26:21 +0200

    update social icons

--- src/components/Navbars/IndexNavbar.vue
+++ src/components/Navbars/IndexNavbar.vue
@@ -74,7 +74,7 @@
           <li class="flex items-center">
             <a
               class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-              href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdemos.www.mlabfactory.it%2Fbudget-control%2F%23%2F"
+              href="https://www.facebook.com/profile.php?id=61564453328542"
               target="_blank"
             >
               <i class="text-blueGray-400 fab fa-facebook text-lg leading-lg" />
@@ -85,22 +85,11 @@
           <li class="flex items-center">
             <a
               class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-              href="https://twitter.com/intent/tweet?url=https%3A%2F%2Fdemos.www.mlabfactory.it%2Fbudget-control%2F%23%2F&text=Start%20your%20development%20with%20a%20Free%20Tailwind%20CSS%20and%20VueJS%20UI%20Kit%20and%20Admin.%20Let%20Vue%20Notus%20amaze%20you%20with%20its%20cool%20features%20and%20build%20tools%20and%20get%20your%20project%20to%20a%20whole%20new%20level.%20"
+              href="https://x.com/BudgetControl24"
               target="_blank"
             >
               <i class="text-blueGray-400 fab fa-twitter text-lg leading-lg" />
               <span class="lg:hidden inline-block ml-2">Tweet</span>
-            </a>
-          </li>
-
-          <li class="flex items-center">
-            <a
-              class="hover:text-blueGray-500 text-blueGray-700 px-3 py-2 flex items-center text-xs uppercase font-bold"
-              href="https://github.com/BudgetControl/Core"
-              target="_blank"
-            >
-              <i class="text-blueGray-400 fab fa-github text-lg leading-lg" />
-              <span class="lg:hidden inline-block ml-2">Star</span>
             </a>
           </li>
         </ul>
