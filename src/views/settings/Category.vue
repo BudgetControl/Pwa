c3367523bf2da52ecbd2dd73735da3b2e8f8ae61
Marco De Felice <marco.defelice890@gmail.com>
2024-09-02 16:22:27 +0200

    disabled edit button of category for improvements

--- src/views/settings/Category.vue
+++ src/views/settings/Category.vue
@@ -8,11 +8,11 @@
 
                     <div class="container px-4 mx-auto" v-on:click="openModal(null, null)">
 
-                        <button
+                        <!-- <button
                             class="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                             type="button">
                             {{ $t('labels.add_new_category') }}
-                        </button>
+                        </button> -->
 
                     </div>
 
