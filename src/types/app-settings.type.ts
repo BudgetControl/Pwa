cb04ea3ff40be3cf5b50d42c7ea25c20bf17a9fc
Marco De Felice <marco.defelice890@gmail.com>
2024-09-20 16:23:56 +0200

    WIP

--- src/types/app-settings.type.ts
+++ src/types/app-settings.type.ts
@@ -1,8 +1,11 @@
 import { User } from './user.type';
+import { Workspace } from './workspace.type';
 
 export interface AppSettings {
     currency_id: number;
     payment_type_id: number;
     eye_settings: boolean;
+    current_ws: Workspace;
     user: User;
+    workspaces: Workspace[];
 }
