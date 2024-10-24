befc1430679c26efc82b6d413a02cc6c6586205a
root <marco.defelice@budgetcontrol.cloud>
2024-10-24 19:21:31 +0200

    fixed label on wallet stats

--- src/i18n/es/labels.vue
+++ src/i18n/es/labels.vue
@@ -219,5 +219,6 @@ export default {
     no_voices_found: 'No se encontraron voces',
     workspace_whared_with: 'Espacio de trabajo compartido con',
     user_not_found: 'Usuario no encontrado',
+    end_of_month: 'Fin de mes',
 }
 </script>
--- src/i18n/it/labels.vue
+++ src/i18n/it/labels.vue
@@ -219,5 +219,7 @@ export default {
     no_voices_found: 'Nessuna voce trovata',
     workspace_whared_with: 'Spazio di lavoro condiviso con',
     user_not_found: 'Utente non trovato',
+    end_of_month: 'Fine mese',
+    
 }
 </script>
