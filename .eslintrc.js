b76536a7ec04d03d5c3c13f71d9698308c36bea1
root <marco.defelice@budgetcontrol.cloud>
2024-11-18 09:14:29 +0100

    updated new packages version

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    root: true,
+    env: {
+      node: true,
+    },
+    extends: [
+      'plugin:vue/vue3-recommended', // o essential
+      'eslint:recommended',
+    ],
+    rules: {
+      'vue/multi-word-component-names': 'off', // Disattiva la regola
+    },
+  };
+  
