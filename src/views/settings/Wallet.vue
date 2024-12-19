3b0d7936f4d3eaa60b40f32f1f444d90d40d799d
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2024-12-19 15:59:56 +0100

    fix: remover order by from wallet card

--- src/views/settings/Wallet.vue
+++ src/views/settings/Wallet.vue
@@ -21,8 +21,8 @@
                                 class="flex items-center w-full text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg sm:flex dark:bg-gray-700 dark:border-gray-600 dark:text-white">
                                 <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600 flex lg:w3/12 px-4">
                                     <div class="flex items-center ps-3">
-                                        <input id="vue-checkbox-list" type="checkbox" v-model="action.order" value="true"
-                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                        <!-- <input id="vue-checkbox-list" type="checkbox" v-model="action.order" value="true"
+                                            class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"> -->
                                         <label for="vue-checkbox-list"
                                             class="w-full py-3 ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"> {{ $t('labels.change_order') }}</label>
                                     </div>
