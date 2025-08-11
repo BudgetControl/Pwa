c4797e40d68804900c8a5e82be82bded93772bb9
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-08-11 18:39:39 +0200

    fix loading on search page

--- src/components/Cards/CardSearchEntries.vue
+++ src/components/Cards/CardSearchEntries.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-slate-100 border-0">
-        <loading v-if="isLoading" />
+        <loading :show="isLoading" />
         <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
             <form>
                 <h6 class="text-slate-400 text-sm mt-3 mb-6 font-bold uppercase">
